82950c86aa9e6ac139facaf0aa581a017c7f8b5e
Matthew Jones <matts3371@gmail.com>
2020-02-23 00:52:11 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ get '/newteam' do
 end 
 
 post '/team' do 
-  @team_name = params[:team_name]
+  @name = params[:name]
   @coach = params[:coach]
   @point_guard = params[:point_guard]
   @shooting_guard = params[:shooting_guard]
